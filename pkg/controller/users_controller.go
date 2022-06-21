0ac29990b1996aa68ca22670a90bb453c53d9a7d
gastonharari <harari.gaston@gmail..com>
2022-06-21 19:23:11 -0300

    bad request response

--- pkg/controller/users_controller.go
+++ pkg/controller/users_controller.go
@@ -1,14 +1,39 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/challenge/pkg/helpers"
 	"github.com/challenge/pkg/models"
+	"github.com/challenge/pkg/usecase"
 )
 
 // CreateUser creates a new user
 func (h Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
-	// TODO: Create a New User
-	helpers.RespondJSON(w, models.User{})
+	err := r.ParseForm()
+	if err != nil {
+		//TODO: Handle error
+		return
+	}
+
+	usr := r.PostForm.Get("username")
+	pwd := r.PostForm.Get("password")
+
+	fmt.Println(usr)
+
+	if usr == "" {
+		fmt.Println(usr)
+		helpers.RespondBadRequest(w, "the username is required")
+		return
+	}
+
+	if pwd == "" {
+		fmt.Println(pwd)
+		helpers.RespondBadRequest(w, "the password is required")
+		return
+	}
+
+	userID := usecase.CreateNewUser(usr, pwd)
+	helpers.RespondJSON(w, models.User{ID: userID})
 }
--- pkg/models/user.go
+++ pkg/models/user.go
@@ -2,5 +2,5 @@ package models
 
 type User struct {
 	// TODO: Implement User model
-	ID int64
+	ID int64 `json:"id"`
 }
