277e5bb9d254dce2cd5978b86eaf5876872741bf
gastonharari <harari.gaston@gmail..com>
2022-06-21 18:53:47 -0300

    inital commit

---
+++ pkg/models/health.go
@@ -0,0 +1,6 @@
+package models
+
+type Health struct {
+	// TODO: Implement Health model
+	Health string `json:"health"`
+}
---
+++ pkg/models/user.go
@@ -0,0 +1,6 @@
+package models
+
+type User struct {
+	// TODO: Implement User model
+	ID int64
+}
